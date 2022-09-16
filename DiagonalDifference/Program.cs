6395481153c485c77503ab15bdc44a2a4e3329d8
Jan Bereźnicki <jan.pawel.bereznicki@gmail.com>
2022-09-16 19:46:45 +0200

    Dodaj pliki projektów.

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+internal class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        Console.WriteLine(result);
+    }
+}
+
+internal class Result
+{
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int NumberFromLeftToRight = 0, NumberFromRightToLeft = 0;
+        int collumsFromLeftToRight = 0, collumsFromRightToLeft = arr.Count-1;
+        for (int rows = 0; rows < arr.Count; rows++)
+        {
+            NumberFromLeftToRight += arr[rows][collumsFromLeftToRight];
+            NumberFromRightToLeft += arr[rows][collumsFromRightToLeft];
+            collumsFromLeftToRight++;
+            collumsFromRightToLeft--;
+        }
+        return Math.Abs(NumberFromLeftToRight - NumberFromRightToLeft);
+    }
+}
